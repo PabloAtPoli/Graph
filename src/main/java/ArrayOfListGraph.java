7f51c8005836eba5baf7416fde82bbed60f3d205
Pablo Ortiz <paortiz@elpoli.edu.co>
2022-12-07 07:23:54 -0500

    Some Refactoriing done

--- src/main/java/ArrayOfListGraph.java
+++ src/main/java/ArrayOfListGraph.java
@@ -8,38 +8,38 @@ public class ArrayOfListGraph {
     private int vertices;
     //creates a linked list for the adjacency list of the graph   
 
-    private LinkedList<Integer> adjlist[];
+    private LinkedList<Integer>[] adjList;
     //creating a constructor of the Graph class   
 
     ArrayOfListGraph(int count_v) {
         //assigning the number of vertices to the passed parameter  
         vertices = count_v;
-        adjlist = new LinkedList[count_v];
+        adjList = new LinkedList[count_v];
 
         //loop for creating the adjacency lists  
         for (int i = 0; i < count_v; ++i) {
-            adjlist[i] = new LinkedList();
+            adjList[i] = new LinkedList();
         }
     }
 //method that adds a new edge to the graph  
 
     void addNewEdge(int v, int w) {
-        adjlist[v].add(w);
+        adjList[v].add(w);
     }
 
-    void traversalDFS(int v, boolean vnodelist[]) {
-        //if current node (root node) is visited, add it to the vnodelist   
-        vnodelist[v] = true;
+    void traversalDFS(int v, boolean[] visitedNode) {
+        //if current node (root node) is visited, add it to the visitedNode   
+        visitedNode[v] = true;
         System.out.print(v + " ");
         //determines the negihboring nodes of the current node  
         //iterates over the list    
-        Iterator<Integer> i = adjlist[v].listIterator();
+        Iterator<Integer> i = adjList[v].listIterator();
         while (i.hasNext()) {
             //returns the next element in the iteration and store that element in the variable n      
             int n = i.next();
-            if (!vnodelist[n]) //calling the function that performs depth first traversal    
+            if (!visitedNode[n]) //calling the function that performs depth first traversal    
             {
-                traversalDFS(n, vnodelist);
+                traversalDFS(n, visitedNode);
             }
         }
     }
@@ -57,37 +57,37 @@ public class ArrayOfListGraph {
     void BFS(int rnode) {
         //creates an array of boolean type for visited node   
         //initially all nodes are unvisited  
-        boolean visitednode[] = new boolean[vertices];
+        boolean[] visitedNode = new boolean[vertices];
 
         //creating another list for storing the visited node  
-        LinkedList<Integer> vnodelist = new LinkedList<Integer>();
+        LinkedList<Integer> vNodeList = new LinkedList<Integer>();
 
-        //if current node (root node) is visited, add it to the vnodelist   
-        visitednode[rnode] = true;
+        //if current node (root node) is visited, add it to the vNodeList   
+        visitedNode[rnode] = true;
 
-        //inserts the visited node into vnodelist  
-        vnodelist.add(rnode);
+        //inserts the visited node into vNodeList  
+        vNodeList.add(rnode);
 
         //the while loop executes until we have visited all the nodes  
-        while (vnodelist.size() != 0) {
+        while (vNodeList.size() != 0) {
             //deque an entry from queue and process it    
             //the poll() method retrieves and removes the head (first element) of this list  
-            rnode = vnodelist.poll();
+            rnode = vNodeList.poll();
             System.out.print(rnode + " ");
 
             //detrmines the negihboring nodes of the current node  
             //iterates over the list   
-            Iterator<Integer> i = adjlist[rnode].listIterator();
+            Iterator<Integer> i = adjList[rnode].listIterator();
             while (i.hasNext()) {
                 //returns the next element in the iteration and store that element in the variable n      
                 int n = i.next();
 
                 //checks the next node is visited or not  
-                if (!visitednode[n]) {
+                if (!visitedNode[n]) {
                     //if the above if-statement returns true, visits the node   
-                    visitednode[n] = true;
-                    //adds the visited node in the vnodelist  
-                    vnodelist.add(n);
+                    visitedNode[n] = true;
+                    //adds the visited node in the vNodeList  
+                    vNodeList.add(n);
                 }
             }
         }
